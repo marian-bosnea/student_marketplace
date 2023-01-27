ec4504cf45974c08e3338cee937026abaf48446b
Marian Bosnea <marianbosnea22@gmail.com>
2023-01-27 09:41:00 +0200

    Worked at backend

---
+++ student-marketplace-backend/src/passportConfig.js
@@ -0,0 +1,61 @@
+const LocalStrategy = require('passport-local').Strategy;
+const { pool } = require('./dbConfig');
+const bcrypt = require('bcrypt');
+
+const SELECT_USER_WHERE_EMAIL_QUERY = 'SELECT * FROM users WHERE email = $1';
+const SELECT_USER_WHERE_ID_QUERY = 'SELECT * FROM users WHERE id = $1';
+
+function initialize(passport) {
+    const authenticateUser = (email, password, done) => {
+        pool.query(SELECT_USER_WHERE_EMAIL_QUERY, [email], (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            console.log(result.rows);
+
+            if (result.rows.length > 0) {
+                const user = result.rows[0];
+
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    if (isMatch) {
+                        return done(null, user);
+                    } else {
+                        return done(null, false, { message: "Password is incorrect." });
+                    }
+                });
+            } else {
+                return done(null, false, { message: "Email is not registered" });
+            }
+        }
+        );
+    };
+
+    passport.use(
+        new LocalStrategy(
+            {
+                usernameField: "email",
+                passwordField: "password"
+            },
+            authenticateUser
+        )
+    );
+
+    passport.serializeUser((user, done) => done(null, user.id));
+
+    passport.deserializeUser((id, done) => {
+        pool.query(SELECT_USER_WHERE_ID_QUERY, [id], (err, results) => {
+            if (err) {
+                throw err
+            }
+
+            return done(null, results.rows[0]);
+        });
+    });
+}
+
+module.exports = initialize;
--- student-marketplace-backend/src/server.js
+++ student-marketplace-backend/src/server.js
@@ -1,33 +1,79 @@
-const express = require('express')
+/// Modules
+const express = require('express');
+const { pool } = require("./dbConfig");
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const initializePassport = require('./passportConfig')
+
 const app = express();
-const port = 3000;
 
-const { Client } = require('pg');
+initializePassport(passport);
 
-const client = new Client({
-   user: 'postgres',
-   host: 'localhost',
-   database: 'student-marketplace-db',
-   password: 'haarpcord2233',
-   port: 5432,
+/// Constants
+const PORT = process.env.PORT || 3000;
+const INVALID_INPUT_CODE = 400;
+const POST_SUCCESS_CODE = 201;
+
+// For production
+//app.use(express.urlencoded({extended: false}));
+
+// For Postman testing purposes
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+app.use(bodyParser.json());
+
+
+app.use(passport.session);
+app.use(passport.initialize);
+
+app.post("/users/register", async (req, res) => {
+   let { email, password, password_confirm } = req.body;
+
+   let error_message = "";
+
+   if (!email || !password || !password_confirm) {
+      error_message = "All fields must be filled.";
+   } else if (password.length < 6) {
+      error_message = "Password must be at least 6 characters long.";
+   } else if (password != password_confirm) {
+      error_message = "Entered passwords do not match.";
+   }
+
+   if (error_message.length != 0) {
+      res.send({ body: { message: error_message, status_code: INVALID_INPUT_CODE } });
+   } else {
+      let hashedPassword = await bcrypt.hash(password, 10);
+      const query = 'SELECT * FROM authentication_credentials where email = $1';
+
+      pool.query(query,
+         [email],
+         (err, results) => {
+            if (err) {
+               throw err
+            }
+            if (results.rows.length > 0) {
+               error_message = "Email is associated with other account.";
+               res.send({ body: { message: error_message, status_code: INVALID_INPUT_CODE } });
+            } else {
+               res.send({ body: { status_code: POST_SUCCESS_CODE } });
+            }
+         }
+      )
+   }
 });
 
-client.connect();
+app.post('/users/login'), passport.authenticate('local', {
+   function (req, res) {
+      res.send("Successfuly logged in!");
+   }
+});
 
-client.query("SELECT * FROM user_role").then(res => {
- console.log(res);
- 
-})
-.catch(err => {
-      console.error(err);
-   })
-.finally(() => {
-      client.end();
-   });
+// Listen to specified port
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+}
 
-
-// app.get("/student", (req, res) => {
-//    res.send("Hello client!");
-// });
-
-// app.listen(port, () => console.log('app listening to port ${port}'));
+);
