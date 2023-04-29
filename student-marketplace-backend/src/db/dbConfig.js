8aa85850f9d5652ef7bee3654076ab203a9cd89e
Marian Bosnea <marianbosnea22@gmail.com>
2023-04-29 17:34:40 +0300

    Modifiend db config

--- student-marketplace-backend/src/db/dbConfig.js
+++ student-marketplace-backend/src/db/dbConfig.js
@@ -4,11 +4,11 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
+//const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgres://marianbosnea:Haarpcord2233@unimarketplace-db.postgres.database.azure.com/postgres?sslmode=require`
 console.log(connectionString);
 
-const pool = new Pool ({
+const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString
 });
 
