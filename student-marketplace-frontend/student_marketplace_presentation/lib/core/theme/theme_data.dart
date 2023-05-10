42b93975a272829e8bd84db9410d66d4d0b08207
Bosnea Marian <marianbosnea22@gmail.com>
2023-05-10 19:37:58 +0300

    Modified theme data

--- student-marketplace-frontend/student_marketplace_presentation/lib/core/theme/theme_data.dart
+++ student-marketplace-frontend/student_marketplace_presentation/lib/core/theme/theme_data.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+//Color accentColor = const Color.fromARGB(255, 94, 64, 175);
+Color accentColor = const Color.fromARGB(255, 255, 127, 80);
+
 ThemeData lightTheme() => ThemeData(
       primaryColor: Colors.white,
       highlightColor: const Color.fromARGB(255, 240, 240, 240),
       dividerColor: const Color.fromARGB(255, 200, 200, 200),
-      splashColor: const Color.fromARGB(255, 94, 64, 175),
+      splashColor: accentColor,
       textTheme: GoogleFonts.varelaRoundTextTheme(
         const TextTheme(
             labelLarge: TextStyle(fontSize: 20, color: Colors.black),
@@ -20,9 +23,13 @@ ThemeData lightTheme() => ThemeData(
 
 ThemeData darkTheme() => ThemeData(
       primaryColor: Colors.black,
-      splashColor: const Color.fromARGB(255, 94, 64, 175),
+      splashColor: accentColor,
       highlightColor: const Color.fromARGB(255, 33, 33, 33),
       dividerColor: const Color.fromARGB(255, 130, 130, 130),
+      iconTheme: IconThemeData(color: accentColor),
+      appBarTheme: const AppBarTheme(
+        color: Colors.red,
+      ),
       textTheme: GoogleFonts.varelaRoundTextTheme(
         const TextTheme(
             labelLarge: TextStyle(fontSize: 20, color: Colors.white),
